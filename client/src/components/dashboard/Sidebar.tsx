1aff2f499eebad0adc56cc7a0aaa84aea76a803b
Akshay Sharma <asharma.19042007@gmail.com>
2024-11-24 05:43:55 +0530

    getting all instructor courses implementation

--- client/src/components/dashboard/Sidebar.tsx
+++ client/src/components/dashboard/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { ChartNoAxesColumn, HamIcon, SquareLibrary } from "lucide-react";
+import { ChartNoAxesColumn, SquareLibrary } from "lucide-react";
 import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
